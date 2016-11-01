2db234a2240f5aab331da74684eb4a353b916377
Shiqi Feng <shiqi.f@pervices.com>
2016-11-01 17:54:58 +0000

    FPGA: Updated the script

--- recipes-tools/crimson-fpga/crimson-fpga/update.sh
+++ recipes-tools/crimson-fpga/crimson-fpga/update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 systemctl stop crimson-server
-mkdir fpga_image
+mkdir -p fpga_image
 mount /dev/mmcblk0p1 fpga_image
 cp soc_system.rbf fpga_image/
 sync
 umount fpga_image
 rm -r fpga_image
+systemctl start crimson-server
