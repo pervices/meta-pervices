96dc8ccfc3d626054366b95c0cdf9769cbbb4c47
Shiqi Feng <shiqi.f@pervices.com>
2016-11-09 21:50:30 +0000

    Recipe: Added support script for miscellaneous files

---
+++ recipes-tools/crimson-miscellaneous/crimson-miscellaneous/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+:'
+red='\e[1;31m'
+normal='\e[0m'
+green='\e[1;32m'
+bold='\e[1m'
+
+case "$1" in
+)
+	;;
+)
+	;;
+*)
+	echo -e "$bold Usage: ./update {|} $normal"
+	echo -e "$bold : $normal"
+	echo -e "$bold : $normal"
+	exit 1
+	;;
+esac
+
+exit 0
+'
