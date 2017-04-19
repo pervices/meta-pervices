88bc903b59379598f38ff47ebe1dd4486b230deb
Shiqi Feng <shiqi.f@pervices.com>
2017-04-19 16:53:57 +0000

    MCU: New flash utility and hex files

--- recipes-tools/crimson-mcu/crimson-mcu/flash.sh
+++ recipes-tools/crimson-mcu/crimson-mcu/flash.sh
@@ -1,91 +1,190 @@
 #!/bin/bash
-version=$(tail -1 /etc/crimson/crimson-mcu | cut -d '-' -f2)
 
-if [ $# -lt 1 ]
+function help_summary {
+    echo -e "Usage : $0 [w(rite)|v(erify)] [rx | tx | time | all] \n"
+    echo -e "Example:"
+    echo -e "\t Verify tx board mcu code:"
+    echo -e "\t\t $0 v tx\n"
+    echo -e "\t Write MCU to all boards:"
+    echo -e "\t\t $0 w all\n"
+    exit
+}
+
+PATH_TIME="/dev/ttycrimson-time"
+PATH_RX="/dev/ttycrimson-rx"
+PATH_TX="/dev/ttycrimson-tx"
+
+HEXFILE_TIME="synth.hex"
+HEXFILE_RX="rx.hex"
+HEXFILE_TX="tx.hex"
+
+
+AVRDUDE_BIN="avrdude"
+AVRDUDE_ARGS="-c avr109 -B 8 -p x256a3u -b 115200"
+
+#Note: The current programmer does not support fuses in this mode.
+#This line is here only for reference.
+AVRDUDE_FUSES="-U fuse2:w:0xBE:m  -U fuse4:w:0xF3:m -U fuse5:w:0xE9:m"
+
+#Bootloader specific functions
+BOOTLOADER_LOAD_PATTERN="BOOT"
+BOOTLOADER_ENTRY_SEQUENCE="abcdefghijklmnopqrstuvwxyz\r"
+
+# Prep functions to be carried out prior to programming.
+function start_prep {
+    if [ ! -f /etc/crimson/crimson-mcu ]; 
+    then
+            echo -e "WARNING: Unable to determine version."
+    else
+            version=$(tail -1 /etc/crimson/crimson-mcu | cut -d '-' -f2)
+    fi
+    if [ ! -c $PATH_TIME ] || [ ! -c $PATH_RX ] || [ ! -c $PATH_TX ]; 
+    then
+            echo -e "ERROR: Unable to locate crimson ttys."
+            exit
+    fi
+
+    echo -e "Attempting to disabling crimson-server and crimson-website."
+    sudo systemctl stop crimson-server.service
+    sudo systemctl stop crimson-website.service
+}
+
+# Clean up functions to be executed at the conclusion of programming.
+function end_prep {
+    echo -e "Attempting to restart crimson-server and crimson-website."
+    sudo systemctl start crimson-server.service
+    sudo systemctl start crimson-website.service
+}
+
+# Enable FPGA programming pin.
+function fpga_progpin_enable {
+    echo -e "Enable FPGA driven MCU programing pin."
+    fpgareg=$(mem rr res_rw3)
+    mem rw res_rw3 0 0x1000
+}
+
+# Disable FPGA programming pin.
+function fpga_progpin_disable {
+    echo -e "Disable FPGA driven MCU programing pin."
+    mem rw res_rw3 $fpgareg 0xffffffff
+}
+
+# Common Programming Sequence
+# program_app( $PATH_TO_DEVICE $NAME_OF_HEXFILE)
+function program_app() {
+        PATH_DEV="$1"
+        BOARD_HEX="$2"
+        BOARD_OP="$3"
+
+        fpga_progpin_enable;
+
+	#Attempt to exit bootloader, in case we're in it.
+	timeout 0.3 echo -e "E\r" > $PATH_DEV
+
+        #Clear buffer
+        timeout 0.3 cat $PATH_DEV > /dev/null 
+
+        #Get current time dangerously
+        #There needs to be a better method to doing this then
+        #using the system time, which is mutable.
+        EXIT_TIME=$(( $(date +%s) + 10 ))
+
+        echo -e "Using $BOARD_HEX to program $PATH_DEV\n"
+
+        #Restart MCU
+        echo -e "Issuing reboot command and polling for bootloader reply to $PATH_DEV..."
+        echo -e "boot -e\r" > $PATH_DEV
+
+        #For the next 10s poll the char device and wait for the boot message.
+        while [[ $(( $(date +%s) )) -le $(($EXIT_TIME)) ]];
+        do
+                RESPONSE=$(cat $PATH_DEV)
+                echo $RESPONSE
+                if [[ "$RESPONSE" = *$BOOTLOADER_LOAD_PATTERN* ]];
+                then
+                	if [[ "$BOARD_OP" = "w" ]];
+                	then
+                            echo -e "Start Programming using $BOARD_HEX"
+                            echo -e "$BOOTLOADER_ENTRY_SEQUENCE" > $PATH_DEV
+                            echo $AVRDUDE_BIN $AVRDUDE_ARGS -P $PATH_DEV -e -U flash:$BOARD_OP:$BOARD_HEX
+                            $AVRDUDE_BIN $AVRDUDE_ARGS -P $PATH_DEV -e -U flash:$BOARD_OP:$BOARD_HEX
+                            echo "Completed Verifying $PATH_DEV using $BOARD_HEX"
+                            RESPONSE="marker"
+                        elif [[ "$BOARD_OP" = "v" ]];
+                        then
+                            echo -e "Start Programming using $BOARD_HEX"
+                            echo -e "$BOOTLOADER_ENTRY_SEQUENCE" > $PATH_DEV
+                            echo $AVRDUDE_BIN $AVRDUDE_ARGS -P $PATH_DEV -U flash:$BOARD_OP:$BOARD_HEX
+                            $AVRDUDE_BIN $AVRDUDE_ARGS -P $PATH_DEV -U flash:$BOARD_OP:$BOARD_HEX
+                            echo "Completed Programming $PATH_DEV using $BOARD_HEX"
+                            RESPONSE="marker"
+                        else 
+                            echo -e "ERROR: Invalid Operation specified: $BOARD_OP "
+                        fi
+                        echo -e "exit\r" > $PATH_DEV
+                        echo "installed-${version}-$BOARD_HEX" >> /etc/crimson/crimson-mcu
+                        fpga_progpin_disable
+                        break
+                fi
+        done
+
+        #For safety, redefine the PATH and HEX files to null.
+        PATH_DEV="/dev/null"
+        BOARD_HEX="/dev/null"
+
+        if [[ "$RESPONSE" != 'marker' ]];
+        then
+                echo -e "ERROR: Failed to program $PATH_DEV using $BOARD_HEX\n"
+                return 1
+        else 
+            RESPONSE=""
+        fi
+
+}
+
+# Check number of arguaments
+if [ $# -lt 2 ] || [ $# -gt 2 ]; 
 then
-	echo "Usage : $0 rx | tx | synth | all"
-	exit
+    help_summary
+    return 1
 fi
 
-if [ "$1" != 'synth' ] && [ "$1" != 'tx' ] && [ "$1" != 'rx' ] && [ "$1" != 'all' ]
+if [ "$1" != 'w' ] && [ "$1" != 'v' ]
 then
-	echo "- invalid argument"
-	echo "- Usage : $0 rx | tx | synth | all"	
-	exit
+    help_summary
+    return 1
 fi
 
-
-if [ "$1" = 'synth' ] || [ "$1" = 'all' ]
+if [ "$2" != 'time' ] && [ "$2" != 'tx' ] && [ "$2" != 'rx' ] && [ "$2" != 'all' ]
 then
-	echo "- Programming the synth board MCU..."
-	fpgaprogpin=$(mem rr res_rw3 | head -c 7 | tail -c 1)
-	fpgareg=$(mem rr res_rw3)
-	state=0
-	if [[ $fpgaprogpin != 0 ]]; then
-		mem rw res_rw3 0 0x1000
-		state=1
-	fi
-	echo -e "boot -e\r" > /dev/ttycrimson-time
-	while [[ $(head -n 1 /dev/ttycrimson-time) != *"T"* ]];do :; done
-	echo "Entering Synth Bootloader"
-	usleep 6050000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-time
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-time -b 115200 -e -U flash:w:synth.hex
-	echo -e "exit\r" > /dev/ttycrimson-time
-	echo "- Done programming."
-	echo "installed-${version}-synth" >> /etc/crimson/crimson-mcu
-	if [[ $state != 0 ]]; then
-		mem rw res_rw3 $fpgareg 0xffffffff
-	fi
-	usleep 300000
+    help_summary
+    return 1
 fi
 
-if [ "$1" = 'rx' ] || [ "$1" = 'all' ]
+# All parameters are considered valid, to avoid contention issues, we need shut down
+# the website and server.
+
+# This function MUST preceed all other functions.
+start_prep
+
+BOARD_OPERATON="$1"
+
+if [ "$2" = 'time' ] || [ "$2" = 'all' ]
 then
-	echo "- Programming the rx board MCU..."
-	fpgaprogpin=$(mem rr res_rw3 | head -c 7 | tail -c 1)
-	fpgareg=$(mem rr res_rw3)
-	state=0
-	if [[ $fpgaprogpin != 0 ]]; then
-		mem rw res_rw3 0 0x1000
-		state=1
-	fi
-	echo -e "boot -e\r" > /dev/ttycrimson-rx
-	while [[ $(head -n 1 /dev/ttycrimson-rx) != *"T"* ]];do :; done
-	echo "Entering RX Bootloader"
-	usleep 6050000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-rx
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-rx -b 115200 -e -U flash:w:rx.hex
-	echo -e "exit\r" > /dev/ttycrimson-rx
-	echo "- Done programming."
-	echo "installed-${version}-rx" >> /etc/crimson/crimson-mcu
-	if [[ $state != 0 ]]; then
-		mem rw res_rw3 $fpgareg 0xffffffff
-	fi
-	usleep 300000
+        program_app "$PATH_TIME"  "$HEXFILE_TIME" "$BOARD_OPERATON"
 fi
 
-if [ "$1" = 'tx' ] || [ "$1" = 'all' ]
+if [ "$2" = 'rx' ] || [ "$2" = 'all' ]
 then
-	echo "- Programming the tx board MCU..."
-	fpgaprogpin=$(mem rr res_rw3 | head -c 7 | tail -c 1)
-	fpgareg=$(mem rr res_rw3)
-	state=0
-	if [[ $fpgaprogpin != 0 ]]; then
-		mem rw res_rw3 0 0x1000
-		state=1
-	fi
-	echo -e "boot -e\r" > /dev/ttycrimson-tx
-	while [[ $(head -n 1 /dev/ttycrimson-tx) != *"T"* ]];do :; done
-	echo "Entering TX Bootloader"
-	usleep 6010000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-tx
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-tx -b 115200 -e -U flash:w:tx.hex
-	echo -e "exit\r" > /dev/ttycrimson-tx
-	echo "- Done programming."
-	echo "installed-${version}-rx" >> /etc/crimson/crimson-mcu
-	if [[ $state != 0 ]]; then
-		mem rw res_rw3 $fpgareg 0xffffffff
-	fi
-	usleep 300000
+        program_app "$PATH_RX"  "$HEXFILE_RX" "$BOARD_OPERATON"
 fi
+
+if [ "$2" = 'tx' ] || [ "$2" = 'all' ]
+then
+        program_app "$PATH_TX"  "$HEXFILE_TX" "$BOARD_OPERATON"
+fi
+#
+# This function MUST be the last thing called.
+end_prep;
+
+exit
