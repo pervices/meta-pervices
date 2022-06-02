2b500f1cc1375ea47898d1fc8f9553d57c6c84fd
John Polak <john.p@pervices.com>
2022-06-02 19:35:33 +0000

    EXECSTOP.SH: gracefully power down RFEs before power off or reboot

--- recipes-tools/cyan-filesystem/cyan-filesystem/usr/bin/execstop.sh
+++ recipes-tools/cyan-filesystem/cyan-filesystem/usr/bin/execstop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# gracefully powerdown all RFEs
+for BOARD in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+	rfe_control $BOARD off n
+done
+
+# send reboot or poweroff command to power board
 /bin/systemctl list-jobs | egrep -q 'reboot.target.*start' && i2cset -y 2 0x70 1 0xF7 || i2cset -y 2 0x70 1 0xFB
 
