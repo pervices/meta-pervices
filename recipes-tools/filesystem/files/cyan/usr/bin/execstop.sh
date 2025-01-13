96c348c593a954d5385aca80797d328340d7c885
Amelia MacLatchy <amelia.m@pervices.com>
2025-01-13 18:55:28 +0000

    CRIM,CHES,CYAN: Kill all active SSH sessions on poweroff/reboot

--- recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
+++ recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
@@ -9,3 +9,5 @@ done
 # send reboot or poweroff command to power board
 /bin/systemctl list-jobs | egrep -q 'reboot.target.*start' && i2cset -y 2 0x70 1 0xF7 || i2cset -y 2 0x70 1 0xFB
 
+# kill all open ssh sessions
+pkill sshd
