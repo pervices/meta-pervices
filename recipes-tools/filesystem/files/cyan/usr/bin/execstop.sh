2ce3b60d7e4e02d636dc6e15c01fcf5595ea3883
Amelia MacLatchy <amelia.m@pervices.com>
2025-03-05 17:08:27 +0000

    Revert "CRIM,CHES,CYAN: Kill all active SSH sessions on poweroff/reboot"
    This reverts commit 96c348c593a954d5385aca80797d328340d7c885.

--- recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
+++ recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
@@ -9,5 +9,3 @@ done
 # send reboot or poweroff command to power board
 /bin/systemctl list-jobs | egrep -q 'reboot.target.*start' && i2cset -y 2 0x70 1 0xF7 || i2cset -y 2 0x70 1 0xFB
 
-# kill all open ssh sessions
-pkill sshd
