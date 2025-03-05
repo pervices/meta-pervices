8acc8d7340a70a648406c079bab093124bd5fa51
Amelia MacLatchy <amelia.m@pervices.com>
2025-03-05 17:08:18 +0000

    Revert "CHES,CYAN: Correct order of operations"
    This reverts commit 6ac43676cbeceec20b2ea9649748c50a6db4fb09.

--- recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
+++ recipes-tools/filesystem/files/cyan/usr/bin/execstop.sh
@@ -6,8 +6,8 @@ do
 	rfe_control $BOARD off n
 done
 
-# kill all open ssh sessions
-pkill sshd
-
 # send reboot or poweroff command to power board
 /bin/systemctl list-jobs | egrep -q 'reboot.target.*start' && i2cset -y 2 0x70 1 0xF7 || i2cset -y 2 0x70 1 0xFB
+
+# kill all open ssh sessions
+pkill sshd
